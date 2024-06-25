1b9a240984b890033f083f4f98de824e94e49703
luanfeng <luanfeng@hishop.com>
2024-06-25 13:49:58 +0800

    hot key

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,6 +2,7 @@ use tauri::Manager;
 use tauri_plugin_autostart::{MacosLauncher, ManagerExt};
 use tauri_plugin_cli::CliExt;
 use tauri_plugin_fs::FsExt;
+use tauri_plugin_global_shortcut::GlobalShortcutExt;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -13,64 +14,53 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_http::init())
-        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_cli::init())
+        .plugin(tauri_plugin_autostart::init(
+            MacosLauncher::LaunchAgent,
+            Some(vec!["--flag1", "--flag2"]),
+        ))
+         .plugin(tauri_plugin_shell::init())
         .setup(|app| {
-            println!("im here11"); // not here
 
             #[cfg(desktop)]
             {
-                use tauri_plugin_global_shortcut::{
-                    Code, GlobalShortcutExt, Modifiers, Shortcut, ShortcutState,
-                };
+                use tauri::Manager;
+                use tauri_plugin_global_shortcut::{Code, Modifiers, ShortcutState};
 
-                let ctrl_n_shortcut = Shortcut::new(Some(Modifiers::CONTROL), Code::KeyN);
+                println!("------");
+                let scope = app.fs_scope();
+                scope.allow_directory("/home/workoss", false);
+                dbg!(scope.allowed());
+
+                match app.cli().matches() {
+                    Ok(matches) => {
+                        println!("{:?}", matches)
+                    }
+                    Err(_) => {}
+                }
+                
+
                 app.handle().plugin(
                     tauri_plugin_global_shortcut::Builder::new()
-                        // .with_shortcuts(["ctrl+n", "alt+space"])?
+                        .with_shortcuts(["ctrl+d", "alt+n"])?
                         .with_handler(|app, shortcut, event| {
-                            println!("im here"); // not here
-                            if event.state == ShortcutState::Pressed {
-                                if shortcut.matches(Modifiers::CONTROL, Code::KeyN) {
-                                    println!("Ctrl+N triggered");
-                                    let _ = app.emit("shortcut-event", "Ctrl+N triggered");
+                            println!("==========");
+                            if event.state == ShortcutState::Pressed  {
+                                if shortcut.matches(Modifiers::CONTROL, Code::KeyD) {
+                                    let _ = app.emit("shortcut-event", "Ctrl+D triggered");
                                 }
-                                if shortcut.matches(Modifiers::ALT, Code::Space) {
-                                    println!("Alt+Space triggered");
-                                    let _ = app.emit("shortcut-event", "Alt+Space triggered");
+                                if shortcut.matches(Modifiers::ALT, Code::KeyN) {
+                                    let _ = app.emit("shortcut-event", "Alt+N triggered");
                                 }
                             }
                         })
                         .build(),
                 )?;
+            }
 
-                app.global_shortcut().register(ctrl_n_shortcut)?;
-            }
-            Ok(())
-        })
-        .plugin(tauri_plugin_fs::init())
-        .setup(|app| {
-            let scope = app.fs_scope();
-            scope.allow_directory("/home/workoss", false);
-            dbg!(scope.allowed());
-            Ok(())
-        })
-        .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_clipboard_manager::init())
-        .plugin(tauri_plugin_cli::init())
-        .setup(|app| {
-            match app.cli().matches() {
-                Ok(matches) => {
-                    println!("{:?}", matches)
-                }
-                Err(_) => {}
-            }
-            Ok(())
-        })
-        .plugin(tauri_plugin_autostart::init(
-            MacosLauncher::LaunchAgent,
-            Some(vec!["--flag1", "--flag2"]),
-        ))
-        .setup(|app| {
             let autostart_manager = app.autolaunch();
             //enable autostart
             let _ = autostart_manager.enable();
@@ -80,9 +70,11 @@ pub fn run() {
             );
             //disable autostart
             let _ = autostart_manager.disable();
+
+
             Ok(())
         })
-        .plugin(tauri_plugin_shell::init())
+       
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
